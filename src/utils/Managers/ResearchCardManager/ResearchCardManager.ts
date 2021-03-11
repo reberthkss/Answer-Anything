aa8328b7f935982ff88190fc2c15e09079c64c77
Reberth <reberthkss@outlook.com>
2021-03-10 21:30:58 -0300

    Added listeners to researc card manager

--- src/utils/Managers/ResearchCardManager/ResearchCardManager.ts
+++ src/utils/Managers/ResearchCardManager/ResearchCardManager.ts
@@ -1,13 +1,45 @@
+import {app} from "../../../index";
+import {FirestoreManager} from "../../Services/FirebaseManager/FirestoreManager";
+import {AnswerData} from "../../Data/AnswerData";
+import {useDispatch} from "react-redux";
+import {saveAnswersOfResearch} from "../../../redux/Actions";
+
+interface ResearchCardManagerResponse {
+    response: boolean,
+    error: null | string
+}
+
 export class ResearchCardManager {
-    constructor() {
+    private onChangeListener: any;
+    private firestore = app.firestore;
+
+    async setListener(researchId: string): Promise<ResearchCardManagerResponse> {
+        const dispatch = useDispatch();
+        try {
+            this.onChangeListener = (await  this.firestore()
+                .collection(FirestoreManager.COLLECTIONS.RESEARCH)
+                .where("researchId.researchId", "==", researchId)
+                .get())
+                .docs[0]
+                .ref
+                .collection(FirestoreManager.COLLECTIONS.ANSWERED_QUESTIONS)
+                .onSnapshot((data) => {
+                    const answerData = AnswerData.from(researchId, data.docs[0]);
+                    console.log(`Answer Data => ${answerData}`);
+                })
+            return {response: true, error: null}
+        } catch (e) {
+            return {response: false, error: e.message}
+        }
     }
 
-    setListener() {
-
-    }
-
-    removeListener() {
-
+    async removeListener(): Promise<ResearchCardManagerResponse> {
+        try {
+            this.onChangeListener();
+            return {response: true, error: null}
+        } catch (e) {
+            return {response: false, error: e.message}
+        }
     }
 }
 
