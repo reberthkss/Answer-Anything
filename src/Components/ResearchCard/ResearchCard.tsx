669a8ff09e9cce4b893ce8ed8214a26680683554
Reberth <reberthkss@outlook.com>
2021-03-12 00:17:13 -0300

    Added Research Card

--- src/Components/ResearchCard/ResearchCard.tsx
+++ src/Components/ResearchCard/ResearchCard.tsx
@@ -1,5 +1,26 @@
-const ResearchCard = () => {
+import {ResearchProps} from "../../redux/reducer";
+import {useEffect} from "react";
+import ResearchCardManager from "../../utils/Managers/ResearchCardManager/ResearchCardManager";
+import React from "react";
+import "./ResearchCard.css"
+interface ResearchCardProps {
+    researchId: string,
+    title: string
+}
 
+const ResearchCard = ({researchId, title}: ResearchCardProps) => {
+    const researchCardManager = new ResearchCardManager(researchId);
+    useEffect(() => {
+        researchCardManager.setListener();
+        return () => {
+            researchCardManager.removeListener()
+        }
+    }, []);
+    return (
+        <div className={"research-card-root-container"}>
+
+        </div>
+    )
 }
 
 export default ResearchCard;
