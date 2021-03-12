e9845885a2d62d43fee768089211c7f938f36ea8
Reberth <reberthkss@outlook.com>
2021-03-12 19:08:39 -0300

    Added chart, title and description to research card

--- src/Components/ResearchCard/ResearchCard.tsx
+++ src/Components/ResearchCard/ResearchCard.tsx
@@ -1,24 +1,69 @@
-import {ResearchProps} from "../../redux/reducer";
+import {ReduxState, ResearchProps} from "../../redux/reducer";
 import {useEffect} from "react";
 import ResearchCardManager from "../../utils/Managers/ResearchCardManager/ResearchCardManager";
 import React from "react";
 import "./ResearchCard.css"
+import {useSelector} from "react-redux";
+import {Card, CardContent, CardHeader, IconButton, Typography} from "@material-ui/core";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import ComputedAnswersManager from "../../utils/Managers/ComputedAnswersManager/ComputedAnswersManager";
 interface ResearchCardProps {
     researchId: string,
-    title: string
+    title: string,
+    height: number
 }
 
-const ResearchCard = ({researchId, title}: ResearchCardProps) => {
+const ResearchCard = ({researchId, title, height}: ResearchCardProps) => {
     const researchCardManager = new ResearchCardManager(researchId);
+    const computedResearchManager = new ComputedAnswersManager(researchId);
+    const research = useSelector((state: ReduxState) => state.researchs.find((research) => research.researchId == researchId));
+    if (!research) throw  Error("Research not found!");
     useEffect(() => {
-        researchCardManager.setListener();
+        researchCardManager.subscribe();
+        computedResearchManager.subscribe();
         return () => {
-            researchCardManager.removeListener()
+            researchCardManager.unsubscribe();
+            computedResearchManager.unsubscribe();
         }
     }, []);
-    return (
-        <div className={"research-card-root-container"}>
 
+    const renderChart = () => {
+        if (research.computedAnswers) {
+            const chartData = computedResearchManager.parseDataToChart({researchId: research.researchId, computedAnswers: research.computedAnswers});
+
+        } else {
+            return (
+                <div className={"research-card-no-available-data-container"}>
+                    <Typography>
+                        No available data!
+                    </Typography>
+                </div>
+            )
+        }
+    }
+
+    return (
+        <div className={"research-card-root-container"} style={{height: height}}>
+            <Card>
+                <CardHeader
+                    title={research.research.title}
+                    subheader={research.research.subtitle}
+                    action={
+                        <IconButton aria-label="settings">
+                            <MoreVertIcon />
+                        </IconButton>
+                    }
+                />
+                <CardContent>
+                    {renderChart()}
+                </CardContent>
+                <CardContent>
+                    <Typography>
+                        {research.research.description}
+                    </Typography>
+                </CardContent>
+
+            </Card>
         </div>
     )
 }
