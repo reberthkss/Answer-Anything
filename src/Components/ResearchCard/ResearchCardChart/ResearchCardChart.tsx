0cb03e62b5e33bed7883d83286835b7b5b80bafe
Reberth <reberthkss@outlook.com>
2021-03-13 15:37:22 -0300

    Created Research Card Chart

---
+++ src/Components/ResearchCard/ResearchCardChart/ResearchCardChart.tsx
@@ -0,0 +1,63 @@
+import {ComputedAnswers} from "../../../utils/Data/ComputedAnswers";
+import {Card, Typography} from "@material-ui/core";
+import React from "react";
+import ComputedAnswersChartConverters from "../../../utils/ChartConverters/ComputedAnswersChartConverters";
+import {ChartWrapper} from "../../Chart/ChartWrapper";
+import {Research} from "../../../utils/Data/ResearchData";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import "./ResearchCardChart.css"
+interface ResearchCardChartProps {
+    research: Research,
+    computedAnswers: ComputedAnswers | null
+}
+
+const ResearchCardChart = ({research, computedAnswers}: ResearchCardChartProps) => {
+    const renderNoDataAvailable = () => {
+        return (
+            <div className={"research-card-no-available-data-container"}>
+                <Typography>
+                    No available data!
+                </Typography>
+            </div>
+        )
+    }
+
+    if (computedAnswers) {
+        const chartConverter = new ComputedAnswersChartConverters(computedAnswers, research);
+        if (chartConverter.chartData) {
+            return <Carousel
+                showThumbs={false}
+                renderIndicator={((clickHandler, isSelected, index, label) => {
+                    return (
+                        <span
+                            onClick={clickHandler}
+                            style={{padding: 5,
+                                margin: 5,
+                                fontSize: 1,
+                                borderRadius: 50,
+                                color: "transparent",
+                                backgroundColor: isSelected ? "#3F51B5" : "#000",
+                                boxShadow: ""
+                            }}>{index}</span>
+                    )
+                })}
+                statusFormatter={((currentItem, total) => `Questão ${currentItem} de ${total}`)}
+                emulateTouch={true}
+            >
+                {chartConverter.chartData.map((data) => (
+                    <div style={{paddingBottom: 25, backgroundColor: "white"}}>
+                        <ChartWrapper data={data.data} backgroundColors={data.backgroundColors} labels={data.labels}
+                                      title={data.title} type={"bar"}/>
+                    </div>
+                ))}
+            </Carousel>
+        } else {
+            return renderNoDataAvailable();
+        }
+    } else {
+        return renderNoDataAvailable();
+    }
+}
+
+export default ResearchCardChart;
